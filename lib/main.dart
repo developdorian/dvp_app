199445dc6bc3ff30363116643c2069f412afc7d9
Develop Dorian <developdorian@gmail.com>
2025-11-09 20:39:10 -0500

    feat: add core architecture and app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'core/router/app_router.dart';
+import 'features/form/data/models/user_model.dart';
+import 'features/form/data/models/address_model.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Hive.initFlutter();
+  
+  Hive.registerAdapter(UserModelAdapter());
+  Hive.registerAdapter(AddressModelAdapter());
+  
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +21,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp.router(
+      title: 'DVP App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
+      routerConfig: appRouter,
     );
   }
 }
